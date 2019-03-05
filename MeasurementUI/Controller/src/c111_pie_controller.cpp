c982e3aa00260a17200a30e39d1d7f6497f348d6
Wen Long Wang <wenlong.wang@polito.it>
2019-03-05 18:30:21 +0100

    0.1.2f Add pie chart for target capacity. Now application is able to switch between PWM discharging and constant current discharging.

--- MeasurementUI/Controller/src/c111_pie_controller.cpp
+++ MeasurementUI/Controller/src/c111_pie_controller.cpp
@@ -66,7 +66,13 @@ void Pie_Controller::init(QString chart_title)
  ******************************************************************************/
 void Pie_Controller::setUsed_capacity(double current_capacity)
 {
-    double used_percentage = current_capacity/_target_capacity;
+    double used_percentage;
+
+    if(current_capacity <_target_capacity ){
+        used_percentage = current_capacity/_target_capacity;
+    } else {
+        used_percentage = 1;
+    }
     _slice0->setValue(used_percentage);
     _slice1->setValue(1-used_percentage);
 }
